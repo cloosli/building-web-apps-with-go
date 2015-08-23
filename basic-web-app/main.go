687087e5d2ba4879c03cf9645cf16b75e35e036c
Christian Loosli <loosli.christian@gmail.com>
2015-08-23 22:32:04 +0200

    gofmt

--- basic-web-app/main.go
+++ basic-web-app/main.go
@@ -8,11 +8,11 @@ import (
 )
 
 func main() {
-	port:=os.Getenv("PORT")
-	if port=="" {
-		port="8080"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
-	
+
 	http.HandleFunc("/markdown", GenerateMarkdown)
 	http.Handle("/", http.FileServer(http.Dir("public")))
 	http.ListenAndServe(":"+port, nil)
